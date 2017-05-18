535409ee1863d3c22117fa22e0ee103a1933b655
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2017-05-18 15:40:19 +0530

    Added linkedin bot

---
+++ linkedin/src/payload.js
@@ -0,0 +1,37 @@
+console.log('loaded!!!!!');
+
+var countOfPagesScrolled = 0;
+var searchPhrase = "Developer";
+
+function scrollDown(height, countOfPagesScrolled){
+	scroll(0, document.body.clientHeight);
+	setTimeout(function(){
+		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
+			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
+		}else
+			return sendRequest();
+	}, 1500);
+}
+
+function sendRequest(){
+	var contactsNum = 0;
+	$.each( $('div.mn-person-card__card-actions button.mn-person-card__person-btn-ext'), 	function() {
+		$(this).each(function() {
+			  $.each(this.attributes, function() {
+			    if(this.specified) {
+				    if (this.name.indexOf("data-ember-action-") >= 0){
+					var allInf = $("#ember"+(this.value-4)).text();
+					if (allInf.indexOf(searchPhrase) >= 0){
+						console.log($("#ember"+(this.value-4)).text());
+						contactsNum++;
+						$(this).click();
+					}
+				    }
+			    }
+			  });
+			});
+	});
+	console.log('Just added contacts: ' + contactsNum);
+}
+
+scrollDown(document.body.clientHeight, countOfPagesScrolled);
