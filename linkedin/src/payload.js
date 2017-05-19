5bce565a7a055133c985e9979dc6adf663f0f1f3
Vaibhavdixit02 <vaibhavyashdixit@gmail.com>
2017-05-19 13:10:41 +0530

    Linkedin

--- linkedin/src/payload.js
+++ linkedin/src/payload.js
@@ -1,37 +1,35 @@
 console.log('loaded!!!!!');
 
-var countOfPagesScrolled = 0;
-var searchPhrase = "Developer";
+var counter = 0;
 
-function scrollDown(height, countOfPagesScrolled){
-	scroll(0, document.body.clientHeight);
-	setTimeout(function(){
-		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
-			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
-		}else
-			return sendRequest();
-	}, 1500);
-}
+ function sendrequest(){
+ 	$('li.search-result').each(function() {
+ 		// var details = $(this).find('div.search-result__info').text();
+ 		var details = $(this).text();
+ 		console.log(details);
+ 		console.log(this);
 
-function sendRequest(){
-	var contactsNum = 0;
-	$.each( $('div.mn-person-card__card-actions button.mn-person-card__person-btn-ext'), 	function() {
-		$(this).each(function() {
-			  $.each(this.attributes, function() {
-			    if(this.specified) {
-				    if (this.name.indexOf("data-ember-action-") >= 0){
-					var allInf = $("#ember"+(this.value-4)).text();
-					if (allInf.indexOf(searchPhrase) >= 0){
-						console.log($("#ember"+(this.value-4)).text());
-						contactsNum++;
-						$(this).click();
-					}
-				    }
-			    }
-			  });
-			});
-	});
-	console.log('Just added contacts: ' + contactsNum);
-}
+ 		counter++;
+ 		console.log("count is " + counter);
 
-scrollDown(document.body.clientHeight, countOfPagesScrolled);
+ 		// var b = $(this).find('button.search-result__actions--primary');
+ 		// $(b).click();
+
+ 		// var c = $(this).find('button.button-primary-large');
+ 		// $(c).click();
+
+ 		// var c = $(this).find('send-invite__cancel-btn');
+ 		// setTimeout(function(){
+ 		// $(c).click();
+ 		// }, 1000);
+
+ 	});
+ // 	setTimeout(function(){
+	// 	$("button.next").click();
+	// }, 2000);
+ 	
+ }
+
+ $(document).ready(function() {
+ sendrequest();
+});
