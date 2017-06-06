2a45c4082436d88aec3a537d314b1561c371b440
Shubhajit Saha <suvozit@icloud.com>
2017-06-06 21:37:05 +0530

    tab to spaces @Vaibhavdixit02 use 4 spaces

--- linkedin/src/payload.js
+++ linkedin/src/payload.js
@@ -12,20 +12,20 @@ localforage.config({
   description : 'some description'
 });
 
-function sendrequest(interval = 500){
+function sendrequest(interval){
 
- 	//taking the details one person a time
- 	$('li.search-result').each(function() {
- 		var details = $(this).find('div.search-result__info').text();
+    //taking the details one person a time
+    $('li.search-result').each(function() {
+        var details = $(this).find('div.search-result__info').text();
         details = details.replace(/\s+/g, '');
- 		// details = $(this).text().replace(/ /g,'');
- 		// console.log(details);
- 		// console.log(this);
- 		
- 		//counting the number of results in a page
- 		counter++;
- 		//storing the count in local storage
- 		localforage.setItem('counter', counter);
+        // details = $(this).text().replace(/ /g,'');
+        // console.log(details);
+        // console.log(this);
+        
+        //counting the number of results in a page
+        counter++;
+        //storing the count in local storage
+        localforage.setItem('counter', counter);
 
         var b = $(this).find('button.search-result__actions--primary.button-secondary-medium.m5');
         setTimeout( function(){ $(b).click(); }, interval);
@@ -42,50 +42,50 @@ function sendrequest(interval = 500){
 
         }, interval);
         interval += 3000;
- 	});
+    });
 
     console.log("total request sent: " + counter);
 
- 	//going to the next page
- 	setTimeout(function(){
+    //going to the next page
+    setTimeout(function(){
         console.log("go to next page");
 
-		$("button.next").click();
-	}, interval+1000);
+        $("button.next").click();
+    }, interval+2000);
  }
 
  $(document).ready(function() {
- 	
- 	//going to the local storage to get the current count
-	localforage.getItem('counter',function(err, value){
-		if( isNaN(value)){
-			counter =0;
-			localforage.setItem('counter', counter);
-		}
-		else{
-			counter = value;
-		}
+    
+    //going to the local storage to get the current count
+    localforage.getItem('counter',function(err, value){
+        if( isNaN(value)){
+            counter =0;
+            localforage.setItem('counter', counter);
+        }
+        else{
+            counter = value;
+        }
 
-		//only send maxcount request at one time
-		if(counter < maxcount){
-	 		scrollDown(document.body.clientHeight,1);
-		}
+        //only send maxcount request at one time
+        if(counter < maxcount){
+            scrollDown(document.body.clientHeight,1);
+        }
         else console.log('max request sent ' + maxcount + '!!!');
-	});
+    });
 });
 
 //scrolling function
 function scrollDown(height, countOfPagesScrolled){
-	scroll(0, document.body.clientHeight);
-	setTimeout(function(){
-		if(height != document.body.clientHeight && countOfPagesScrolled > 0){
+    scroll(0, document.body.clientHeight);
+    setTimeout(function(){
+        if(height != document.body.clientHeight && countOfPagesScrolled > 0){
 
             console.log('scroll to bottom');
-			scrollDown(document.body.clientHeight, --countOfPagesScrolled);
-		}
+            scrollDown(document.body.clientHeight, --countOfPagesScrolled);
+        }
         else{
 
-			return sendrequest();
+            return sendrequest(2000);
         }
-	}, 3000);
+    }, 2000);
 }
